a7f32275786b2fa5bba17d11fb95285c7a557e6c
jj2021 <james.jenkins2@marist.edu>
2021-10-30 13:56:12 -0400

    Add Route For Blog Posts
    Add dynamic route for individual blog posts. Use static rendering to
    render the title, id, and date of the post.

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,45 @@
-export default function Posts() {
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { getAllPostIds, getPostData, getSortedPostsData } from '../../lib/posts'
+import Head from 'next/head'
+import Navigation from '../../components/navigation'
 
+export default function Post({ postData }) {
+  return(
+    <div>
+      <Head>
+        <title>JJDev</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navigation/>
+
+      <main>
+        <h1>
+            {postData.title}
+        </h1>
+        <div>
+            {postData.id}
+            <br/>
+            {postData.date}
+        </div>
+      </main>
+    </div>
+  )
 }
 
-export async function getStaticPaths() {
-
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
 }
 
-export async function getStaticProps({params}) {
-
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    const postData = getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
 }
