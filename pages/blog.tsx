25494a8fcb1aa79aab399cc9ada2c1a397f54e25
jj2021 <james.jenkins2@marist.edu>
2021-11-26 12:33:13 -0500

    Populate Blog Page
    Populate blog page with a list of all posts.

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/blog.module.css'
 import Navigation from '../components/navigation'
+import Date from '../components/date'
+import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Blog() {
+export default function Blog({ allPostsData }) {
   return(
     <div>
       <Head>
@@ -17,8 +20,30 @@ export default function Blog() {
           Welcome to the blog!
         </h1>
         <div>
+            <ul>
+                {allPostsData.map(({ id, data}) => (
+                    <li key={id}>
+                        <Link href={`/posts/${id}`}>
+                            <a>{data.title}</a>
+                        </Link>
+                        <br/>
+                        <small>
+                            <Date dateString={data.date} />
+                        </small>
+                    </li>
+                ))}
+            </ul>
         </div>
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,13 +2,14 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 import { getAllPostIds, getPostData, getSortedPostsData } from '../../lib/posts'
 import Head from 'next/head'
 import Navigation from '../../components/navigation'
+import Date from '../../components/date'
 import styles from '../../styles/post.module.css'
 
 export default function Post({ postData }) {
   return(
     <div>
       <Head>
-        <title>JJDev</title>
+        <title>{postData.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -20,7 +21,7 @@ export default function Post({ postData }) {
         </h1>
         <div>
             <h3>{postData.id}</h3>
-            <h3>{postData.date}</h3>
+            <h3><Date dateString={postData.date}/></h3>
         </div>
         <div className={styles.article} dangerouslySetInnerHTML={{ __html: postData.contentHtml }}/>
       </main>
